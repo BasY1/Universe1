50230ce5ac48f7b715ee41e4800f47b722536258
BasY1 <michal.steller@gmail.com>
2025-03-30 11:14:22 +0200

    Update main.cpp - Reset to default method

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include <QApplication>
+
+/*!
+ * \brief The \b main procedure
+ * \param argc Argument count
+ * \param argv Argument array
+ * \return \c EXIT_SUCCESS or \c EXIT_FAILURE
+ */
 int main(int argc, char *argv[])
 {
-    Q_UNUSED(argc)
-    Q_UNUSED(argv)
+    QApplication app(argc, argv);
+    Q_UNUSED(app)
+
     return EXIT_SUCCESS;
 }
